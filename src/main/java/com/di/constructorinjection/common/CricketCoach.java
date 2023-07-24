95184ce07f6e6fd7550204cbd25fcbd5bacdcd1e
musabeytekin <02190201003@ogr.inonu.edu.tr>
2023-07-24 21:16:42 +0300

    bean scope

--- src/main/java/com/di/constructorinjection/common/CricketCoach.java
+++ src/main/java/com/di/constructorinjection/common/CricketCoach.java
@@ -1,8 +1,11 @@
 package com.di.constructorinjection.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) // her di için ayrı nesne oluşturur.
 public class CricketCoach implements Coach{
 
     public CricketCoach() {
--- src/main/java/com/di/constructorinjection/rest/DemoController.java
+++ src/main/java/com/di/constructorinjection/rest/DemoController.java
@@ -9,17 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach coach;
+    private Coach anotherCoach;
+
 
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach coach) {
+    public DemoController(@Qualifier("cricketCoach") Coach coach, @Qualifier("cricketCoach") Coach anotherCoach) {
         System.out.println("In constructor:" + this.getClass().getSimpleName());
         this.coach = coach;
+        this.anotherCoach = anotherCoach;
     }
 
 
     @GetMapping("/getDailyWorkout")
     public String getDailyWorkout() {
         return coach.getDailyWorkout();
+    }
+
+    @GetMapping("check")
+    public String check() {
+        return "Comparing beans: coach==anotherCoach :" + (coach == anotherCoach);
     }
 
 }
